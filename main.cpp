885699b2ff03f0ebadbcc009c9650889cf5a5fe0
RGaming342 <115285880+RGaming342@users.noreply.github.com>
2025-03-21 10:13:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 //args = "main.cpp -lSDL2 -lSDL2_image -lfreetype -lassimp core/logger/logger.o src/*.o src/system/*.o"
 //mk-exe args "main"
 //END
-//https://www.youtube.com/watch?v=it0lsREGdmc&list=PLWTXKdBN8RZe3ytf6qdR4g1JRy0j-93v9
-//https://www.youtube.com/watch?v=o7CHV8Osx-M
 #include <iostream>
 #include "include/title-menu.h"
 
